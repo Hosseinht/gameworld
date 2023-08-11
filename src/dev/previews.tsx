d7fa6ef989256aa6e3fd812ca39e2416f03321e2
Ryan <sailantht@gmail.com>
2023-08-11 15:16:53 +0330

    Color mode & color mode switch

--- src/dev/previews.tsx
+++ src/dev/previews.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import {Previews} from "@react-buddy/ide-toolbox";
+import {ComponentPreview, Previews} from "@react-buddy/ide-toolbox";
 import {PaletteTree} from "./palette";
+import App from "../App";
+import ColorModeSwitch from "../components/ColorModeSwitch";
+import Navbar from "../components/Navbar";
 
 const ComponentPreviews = () => {
     return (
         <Previews palette={<PaletteTree/>}>
+            <ComponentPreview path="/App">
+                <App/>
+            </ComponentPreview>
+            <ComponentPreview path="/ColorModeSwitch">
+                <ColorModeSwitch/>
+            </ComponentPreview>
+            <ComponentPreview path="/Navbar">
+                <Navbar/>
+            </ComponentPreview>
         </Previews>
     );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {ChakraProvider} from '@chakra-ui/react'
+import {ChakraProvider, ColorModeScript} from '@chakra-ui/react'
 import App from './App'
+import theme from "./theme";
 import './index.css'
 import {DevSupport} from "@react-buddy/ide-toolbox";
 import {ComponentPreviews, useInitial} from "./dev";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <ChakraProvider>
+        <ChakraProvider theme={theme}>
             <DevSupport ComponentPreviews={ComponentPreviews}
                         useInitialHook={useInitial}
             >
+                <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
                 <App/>
             </DevSupport>
         </ChakraProvider>
